3419dbe92ae1cc1daed618ebe3afa54642a555ce
ming <chm.wang@hotmail.com>
2025-03-28 16:34:12 +0100

    add comments explaining the bug

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -8,6 +8,12 @@ public class Flik {
      *  @param b Value 2
      *  @return Whether a and b are the same */
     public static boolean isSameNumber(Integer a, Integer b) {
+        /**
+         * To compare two Integers (not ints), the addresses of each other are compared
+         * Java caches the values in [-128, 127], true because they point to the same address
+         * Out of the range, new objects will be created, for example: new Integer(128). So addresses are different
+         */
+
         return a.equals(b);
     }
 }
